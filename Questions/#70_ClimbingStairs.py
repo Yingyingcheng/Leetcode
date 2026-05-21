929d497f0b6062076b09361fccd0e04ef11844f2
Yingyingcheng <doris82717@gmail.com>
2026-05-21 14:13:51 -0700

    #70_ClimbingStairs

---
+++ Questions/#70_ClimbingStairs.py
@@ -0,0 +1,44 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps.
+# In how many distinct ways can you climb to the top?
+
+
+# Example 1:
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+# Example 2:
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+## Using my familiar one DP (Bottom - Up)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        dp = [0] * (n + 1) ## n = 5, dp = [0, 0, 0, 0, 0, 0]
+
+        if n < 2:
+            return n
+        
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1): 
+
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+
+if __name__ == "__main__":
+    print(Solution().climbStairs(2))
+    print(Solution().climbStairs(3))
+    print(Solution().climbStairs(5))
+
