e87e5a709948cc5ff5fb47a292955dc061f99bb0
Yingyingcheng <doris82717@gmail.com>
2026-06-12 20:48:31 -0700

    #128_LongestConsecutiveSequence

--- Questions/#128_LongestConsecutiveSequence.py
+++ Questions/#128_LongestConsecutiveSequence.py
@@ -15,3 +15,33 @@
 # Example 3:
 # Input: nums = [1,0,1,2]
 # Output: 3
+
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        numSet = set(nums)  # 100,4,200,1,3,2
+
+        longest = 0
+
+        for num in numSet:
+
+            if num - 1 not in numSet:
+
+                length = 1
+
+                while (num + length) in numSet:
+
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+
+
+if __name__ == "__main__":
+    print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
+    print(Solution().longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+    print(Solution().longestConsecutive([1, 0, 1, 2]))
