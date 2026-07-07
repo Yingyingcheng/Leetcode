012205b75e2d0ff06393630c986d5eb423bc3332
Yingyingcheng <doris82717@gmail.com>
2026-07-07 11:39:11 -0700

    #78_Subsets

--- Questions/#78_Subsets.py
+++ Questions/#78_Subsets.py
@@ -3,17 +3,13 @@
 # The solution set must not contain duplicate subsets. 
 # Return the solution in any order.
 
- 
-
 # Example 1:
 # Input: nums = [1,2,3]
 # Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-
 # Example 2:
 # Input: nums = [0]
 # Output: [[],[0]]
  
-
 # Constraints:
 # 1 <= nums.length <= 10
 # -10 <= nums[i] <= 10
