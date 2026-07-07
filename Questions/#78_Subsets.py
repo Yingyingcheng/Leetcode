ac574a79a54d9ce875332d78c205d9a0a152a3e6
Yingyingcheng <doris82717@gmail.com>
2026-07-07 11:38:37 -0700

    78_Subsets

---
+++ Questions/#78_Subsets.py
@@ -0,0 +1,66 @@
+# Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+# The solution set must not contain duplicate subsets. 
+# Return the solution in any order.
+
+ 
+
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [[],[0]]
+ 
+
+# Constraints:
+# 1 <= nums.length <= 10
+# -10 <= nums[i] <= 10
+# All the numbers of nums are unique.
+
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+
+        def dfs(i, curSubset):
+            
+            if i >= len(nums):
+
+                res.append(curSubset[:])
+                return 
+            
+            curSubset.append(nums[i])
+            dfs(i + 1, curSubset)                 
+            
+            curSubset.pop()
+            dfs(i + 1, curSubset) 
+
+                ## dfs(1, [1])
+                ##      dfs(2, [1, 2])
+                ##          dfs(3, [1, 2, 3]) base case 
+                ##          dfs(3, [1, 2]) base case 
+                ##      dfs(2, [1])
+                ##          dfs(3, [1, 3]) base case 
+                ##          dfs(3, [1]) base case 
+                ## dfs(1, [])
+                ##      dfs(2, [2])
+                ##          dfs(3, [2, 3]) base case 
+                ##          dfs(3, [2]) base case 
+                ##      dfs(2, [])
+                ##          dfs(3, [3]) base case 
+                ##          dfs(3, []) base case 
+
+        dfs(0, [])
+        return res
+    
+
+if __name__ == "__main__":
+    print(Solution().subsets([1,2,3]))
+    print(Solution().subsets([0]))
+
+
+
